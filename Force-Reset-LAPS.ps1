019bbfc601f674ae5c309e53d4792fcfef30628d
@blake_r38 <39965542+crash0ver1d3@users.noreply.github.com>
2020-09-21 09:32:51 -0500

    Update Force-Reset-LAPS.ps1
    Updated purpose in script comments

--- Force-Reset-LAPS.ps1
+++ Force-Reset-LAPS.ps1
@@ -3,7 +3,7 @@
 #    Date: 2020.09.18
 # Version: 3.5
 #  Author: Blake Regan @blake_r38
-# Purpose: Identify accounts in local groups on domain joined windows workstations
+# Purpose: Force reset LAPS password to sync LAPS clients, or change values in a hurry.
 #   Legal: Script provided "AS IS" without warranties or guarantees of any
 #          kind.  USE AT YOUR OWN RISK.  Public domain, no rights reserved.
 ##############################################################################
